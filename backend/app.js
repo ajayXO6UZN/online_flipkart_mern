258267d4120f610b7e8084d4f6fdb287ddfc9490
ajaySahu <ajay@sahu.edu.in>
2022-01-03 13:39:56 +0530

    bug fixed build path

--- backend/app.js
+++ backend/app.js
@@ -55,10 +55,10 @@ app.use("/api", page);
 app.use("/api", cart);
 app.use("/api", payment);
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../flipkart-template/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "../flipkart-template/build/index.html"));
 });
 
 // Middleware for Errors
